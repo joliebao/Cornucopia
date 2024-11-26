69487c239637767472f7e303cb7dca76bc758d61
jolie <baojolie@gmail.com>
2024-11-25 20:35:07 -0500

    No exceptions

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         System.out.println("Do you want the instructions? (Yes/No) ");
         String confirmation = s.nextLine().toLowerCase();
-        System.out.println();
+
         if (confirmation.equals("yes")){
             Instructions directions = new Instructions();
             System.out.println(directions.instruct());
@@ -16,98 +16,97 @@ public class Main {
         }
         while (!confirmation.equals("no") && !confirmation.equals("yes")){
             System.out.println("Not an option!");
+            System.out.println();
             System.out.println("Do you want the instructions? (Yes/No) ");
             confirmation = s.nextLine().toLowerCase();
-            System.out.println();
         }
 
-//        String result = "";
-        // get grid value (gridNum * gridNum) -- ex (7 by 7)
-        int gridNum = 0;
+        System.out.println();
 
-//        try {
-//            gridNum = s.nextInt();
-//        } catch (NumberFormatException e) {
-//            result = "Isn't num";
-//        }
-//
-//        while (result.equals("Isn't num")){ // just needs parenthesis form
-//                System.out.println("Must be a integer!");
-//                System.out.println("Try again!");
-//                System.out.print("Enter a grid size (integers only): ");
-//                gridNum = s.nextInt();
-//            }
-        while (gridNum < 5 || gridNum > 10) {
+        // get grid value (gridNum * gridNum) -- ex (7 by 7)
+        int gridNum = -100000;
+
+        while (gridNum == -100000 || gridNum < 5 || gridNum > 10) {
             System.out.print("Enter a grid size (integers only): ");
-            gridNum = s.nextInt();
-            if (gridNum < 5) {
+            if (gridNum == -100000){
+                try {
+                    gridNum = Integer.parseInt(s.nextLine());
+                }
+                catch (NumberFormatException e) {
+                    gridNum = 0;
+                    System.out.println("This is not a number!");
+                    System.out.println();
+                }
+            }
+
+            if (gridNum != -100000 && gridNum < 5) {
                 System.out.println("Too small!");
                 System.out.println();
+                gridNum = -100000;
             }
             else if (gridNum > 10){
                 System.out.println("Where do you think you live? A plantation?");
                 System.out.println("Too big!");
                 System.out.println();
+                gridNum = -100000;
             }
         }
 
         int bombNum = 2 + ((gridNum - 5)/2);
+        boolean loop = true;
 
-        // CODE TESTING _____________
+        // instantiates new object
         GridMaker a = new GridMaker(gridNum, bombNum);
 
         System.out.println();
-        boolean loop = true;
+        System.out.println("Find the 'C'!");
+        System.out.println();
         System.out.println(a.shownGrid());
         a.hiddenGrid();
 
+        // Until game ends!
         while (loop){
             System.out.println();
-            System.out.print("Give a coordinate in the form (x,y): ");
-            String g = s.next();
-            a.gridReceiver(g);
+
+            // receive coordinates
+            String coordinate = "not coordinate";
+            String g = "";
+
+            while (coordinate.equals("not coordinate")) {
+                System.out.print("Give a coordinate in the form (x,y): ");
+                try {
+                    g = s.next();
+                    a.gridReceiver(g);
+                    a.getX();
+                    a.getY();
+                    coordinate = "coordinate!";
+                } catch (Exception e) {
+                    coordinate = "not coordinate";
+                    System.out.println("Not in proper coordinate form!");
+                    System.out.println();
+                }
+
+                if (coordinate.equals("coordinate!")) {
+                    a.gridReceiver(g);
+                    int x = a.getX();
+                    int y = a.getY();
+
+                    if (x < 0 || y < 0) {
+                        System.out.println("Too small! Stay inside the maze!");
+                        System.out.println();
+                        coordinate = "not coordinate";
+                    }
+
+                    if (x > gridNum || y > gridNum) {
+                        System.out.println("Too big! Stay inside the maze!");
+                        System.out.println();
+                        coordinate = "not coordinate";
+                    }
+                }
+            }
             System.out.println();
-//
-//            try {
-//                a.GridReceiver(g);
-//                int test = a.getX();
-//                result = "Is coordinate";
-//            } catch (NumberFormatException e) {
-//                result = "Isn't coordinate";
-//            }
-//
-//            if (result.equals("Isn't coordinate")){ // just needs parenthesis form
-//                System.out.println("Must be in the form of (x,y)!");
-//                System.out.println("Try again!");
-//                System.out.print("Give a coordinate in the form (x,y): ");
-//                g = s.nextLine();
-//            }
 
-            if (!(g == "")) {
-                a.gridReceiver(g);
-                int x = a.getX();
-                int y = a.getY();
-                while (x < 0 || y < 0) {
-                    System.out.println("Too small! Stay inside the maze!");
-
-                    System.out.print("Give a coordinate in the form (x,y): ");
-                    g = s.next();
-                    a.gridReceiver(g);
-
-                    x = a.getX();
-                    y = a.getY();
-                }
-                while (x > gridNum || y > gridNum) {
-                    System.out.println("Too big! Stay inside the maze!");
-
-                    System.out.print("Give a coordinate in the form (x,y): ");
-                    g = s.next();
-                    a.gridReceiver(g);
-
-                    x = a.getX();
-                    y = a.getY();
-                }
-
+            if (coordinate.equals("coordinate!")){
                 System.out.println(a.gridChecker());
                 System.out.println(a.changeGrid());
 
